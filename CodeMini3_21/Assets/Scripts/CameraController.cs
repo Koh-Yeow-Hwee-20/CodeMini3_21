6c3bdfccc3df8f40c29ce2942bd9b162897b5656
YeowHwee <yeowhwee13@gmail.com>
2020-11-23 11:50:25 +0800

    Revision

---
+++ CodeMini3_21/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject PlayerModel;
+
+    Vector3 posOffset = new Vector3(3.0f, 2.0f, 2.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, PlayerModel.transform.position + posOffset, 0.1f);
+    }
+}
---
+++ CodeMini3_21/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float speed = 5.0f;
+
+    public Animator playerAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+            startRun();
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+            startRun();
+        }
+
+        if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.W))
+        {
+            playerAnim.SetBool("isRun", false);
+        }
+
+        void startRun()
+        {
+            playerAnim.SetBool("isRun", true);
+            playerAnim.SetFloat("startRun", 0.26f);
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("ConeTag"))
+            {
+                Debug.Log("Activated PlaneB 90Deg rotation");
+            }
+        }
+    }
+}
