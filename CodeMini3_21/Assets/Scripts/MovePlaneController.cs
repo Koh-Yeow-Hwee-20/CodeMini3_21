6bf65bca144807ce318beeca7f4732a7ab4c4189
YeowHwee <yeowhwee13@gmail.com>
2020-11-25 17:08:56 +0800

    homework

---
+++ CodeMini3_21/Assets/Scripts/MovePlaneController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlaneController : MonoBehaviour
+{
+    float zUpperLimit = -20.3f;
+    float zLowerLimit = -32.35f;
+    float moveSpeed = 5.0f;
+
+    bool isMoveFwd = false;
+    bool isMoveBack = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoveBack && !isMoveFwd)
+        {
+            if (transform.position.x >= zLowerLimit)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * moveSpeed);
+            }
+            else
+            {
+                isMoveBack = !isMoveBack;
+                isMoveFwd = !isMoveFwd;
+            }
+        }
+
+        if (isMoveFwd && !isMoveBack)
+        {
+            if(transform.position.x <= zUpperLimit)
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+            }
+            else
+            {
+                isMoveBack = !isMoveBack;
+                isMoveFwd = !isMoveFwd;
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.parent = transform;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.parent = null;
+        }
+    }
+
+
+}
--- CodeMini3_21/Assets/Scripts/PlayerController.cs
+++ CodeMini3_21/Assets/Scripts/PlayerController.cs
@@ -1,22 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     float speed = 5.0f;
+  //public float timer;
 
+    public GameObject PowerUpCollected;
     public Animator playerAnim;
+    public Animator planeB;
+  //public GameObject Timer;
+
+    private int powerUp;
+    private int totalPowerUp;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        totalPowerUp = GameObject.FindGameObjectsWithTag("PowerUp").Length;
+    //  Timer.GetComponent<Text>().text = "Timer: " + timer.ToString("10");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //float verticalInput = Input.GetAxis("Vertical");
+        //float horizontalInput = Input.GetAxis("Horizontal");
+
         if (Input.GetKey(KeyCode.W))
         {
             transform.rotation = Quaternion.Euler(0, -90, 0);
@@ -27,8 +41,18 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.Euler(0, 90, 0);
             startRun();
         }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            startRun();
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            startRun();
+        }
 
-        if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.W))
+        if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
         {
             playerAnim.SetBool("isRun", false);
         }
@@ -40,12 +64,33 @@ public class PlayerController : MonoBehaviour
             transform.Translate(Vector3.forward * Time.deltaTime * speed);
         }
 
-        private void OnTriggerEnter(Collider other)
+        if(transform.position.y < -5)
         {
-            if (other.gameObject.CompareTag("ConeTag"))
-            {
-                Debug.Log("Activated PlaneB 90Deg rotation");
-            }
+            SceneManager.LoadScene("GameOver");
         }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("ConeTag") || powerUp < 4)
+        {
+            Debug.Log("Collect 4 PowerUps first");
+        }
+
+        if (powerUp == 4)
+        {
+            Debug.Log("Activated PlaneB 90Deg rotation");
+            planeB.SetBool("RotateState", true);
+        }
+        if (other.gameObject.tag == "PowerUp")
+        {
+            powerUp++;
+            PowerUpCollected.GetComponent<Text>().text = "PowerUp Collected: " + powerUp;
+
+            Destroy(other.gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        
     }
 }
